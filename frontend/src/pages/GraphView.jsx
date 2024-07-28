8971733f812b87da0cfdb5c168492444a2c0f152
SHRESHTH RAJAN <shreshthrajan@college.harvard.edu>
2024-07-28 13:36:05 -0400

    integrated frontend and backend

---
+++ frontend/src/pages/GraphView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DependencyGraph from '../components/DependencyGraph';
+
+const GraphView = () => (
+  <div>
+    <h2>Dependency Graph</h2>
+    <DependencyGraph />
+  </div>
+);
+
+export default GraphView;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Dependency Extraction Tool</h1>
+  </div>
+);
+
+export default Home;
